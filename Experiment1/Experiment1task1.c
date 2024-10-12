8c5bf047cef7b6bceec8635b146e006088242f53
EmAa-F <emdansari@eng.rizvi.edu.in>
2024-10-12 22:03:13 +0530

    Update Experiment1task1.c

--- Experiment1/Experiment1task1.c
+++ Experiment1/Experiment1task1.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
-/*Aim: Write a program to print your name, age, class, division and UIN on the screen.
-Use escape sequences to properly format the output.*/
+/*
+Aim: Write a program to print your name,     age, class, division and UIN on the screen.
+Use escape sequences to properly format the output.
+
+Name       : Ansari Mohd Emaad Akhtar
+UNI        : 241A019
+Roll No    : 19
+Div        : A1
+Department : Artificial intelligence and data science 
+*/
 
 int main() {
 
-    printf("Student Name\t: Mohd Emaad Akhter\n");
+    printf("Student Name\t: Ansari Mohd Emaad Akhter\n");
     printf("Student UIN ID\t: 241A019\n");
     printf("Student Class\t: FY AIDS\n");
     printf("Student Division: A1\n");
