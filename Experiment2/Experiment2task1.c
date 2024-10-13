8abf86a42c04d5c5730784d3dace58781e91caac
EmAa-F <emdansari@eng.rizvi.edu.in>
2024-10-13 13:01:47 +0530

    Update Experiment2task1.c

--- Experiment2/Experiment2task1.c
+++ Experiment2/Experiment2task1.c
@@ -9,13 +9,23 @@ Department : Artificial intelligence and data science
 */
 #include<stdio.h>
 int main(){
+// output the function of the code
 printf("\t\t\t ***Even odd Finder*** \n\n\n");
-
+//initialization of the variable in data type int(integer)
 int number,remaider ;
 printf("Enter the desired number: ");
+//user input of the number desrired to be checked
 scanf("%d",&number);
+//cslculation of the remainder
 remaider = number%2;
+//main logic using conditional statement
 if(remaider == 0){
+    //will output if the the number is even
     printf("The entered number %d is even",number);
 }
-else{printf("The entered number %d is odd",number);}}
+else{
+    // will output if the number is odd
+    printf("The entered number %d is odd",number);
+}
+return 0;
+}
