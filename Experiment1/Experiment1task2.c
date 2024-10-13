09bed751afe4707047826c9a8350f7c5c8c0f64d
EmAa-F <emdansari@eng.rizvi.edu.in>
2024-10-13 12:49:22 +0530

    Update Experiment1task2.c

--- Experiment1/Experiment1task2.c
+++ Experiment1/Experiment1task2.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /*
-Aim: Write a program to get students PCM marks from the user and find the average. Use conditionaloperator to print if the student is eligible for admission. Eligibility criteria is 50% in
+Aim: Write a program to get students PCM marks from the user and find the average. Use conditional
+operator to print if the student is eligible for admission. Eligibility criteria is 50% in
 PCM.
 Name       : Ansari Mohd Emaad Akhtar
 UNI        : 241A019
@@ -10,18 +11,30 @@ Div        : A1
 Department : Artificial intelligence and data science 
 */
 
-int main() {
-  double P,C,M,PCM;
+int main() { 
+  // initialization of variable in the data type float
+  //p= physics;c= chemistry;m= maths;PCM= avg of the p,c,m value
+  float P,C,M,PCM;
+  // talking input form the user
   printf("Please enter HSC marks\n");
-  printf("Enter Physics marks\t   :");
-  scanf("%lf", &P);
+  printf("Enter Physics marks\t   :")
+  //input physics marks
+  scanf("%f", &P);
   printf("Enter Chemistry marks      :");
-  scanf("%lf", &C);
+  //input chemistry marks
+  scanf("%f", &C);
   printf("Enter Maths marks\t   :");
-  scanf("%lf", &M);
+  //input maths marks
+  scanf("%f", &M);
+  // calculation of the avg of the given marks
   PCM=(P+C+M)/3;
+  //output of the calculated avg
   printf("The Avg of your PCM Marks:%f",PCM);
-  (PCM>=50.0)?printf("\nYou are Eligible for college admission\n"):
+  //main logic using conditional oprators to print if the student is elegibel for college admisson or not
+  (PCM>=50.0)?
+    // will output if avg >= 50%
+    printf("\nYou are Eligible for college admission\n"
+     ):// will output when pcm<=50%
   printf("you are not eligible for college admission");
     return 0;
 }
